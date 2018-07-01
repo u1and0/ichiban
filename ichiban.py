54474cd70d793c32ff97c694dc10a7547307b230
u1and0 <e01.ando60@gmail.com>
2018-07-01 09:53:34 +0900

    [mod]atariくじはkujiメソッドから隠れて使用する, doc test modify

--- ichiban.py
+++ ichiban.py
@@ -2,34 +2,53 @@
 
 
 class Ichiban:
-    """一番くじ確率計算"""
+    """一番くじ確率計算
+    usage:
+        # A賞が1個、B賞が9個残っているくじの計算
+        >>> ich = Ichiban(A=1, B=9)
+        >>> ich.nokori  # 残り景品とその数
+        {'A': 1, 'B': 9}
+        >>> ich.kuji('A')  # A賞を引く確率
+        10.0
 
-    def __init__(self, *hit, **remain):
-        self.atari = [remain[_] for _ in hit]
+        # A,B,C,D賞がそれぞれ1,9,90,100個残っているくじの計算
+        >>> ich = Ichiban(A=1, B=9, C=90, D=100)
+        >>> ich.atari('A', 'B', 'C')  # 当たり景品
+        {'A': 1, 'B': 9, 'C': 90}
+        >>> ich.all()  # 景品の全数
+        200
+
+    args:
+        hit: 当たりくじの賞
+        remain: 現在余っている景品とその数
+    """
+
+    def __init__(self, **remain):
+        """remain: 残り景品とその数
+        >>> Ichiban(S=1, A=2, B=2).nokori
+        {'S': 1, 'A': 2, 'B': 2}
+        """
         self.nokori = remain
 
-    def kuji(self):
-        """当たりくじを引く確率
-        usage:
-            >>> Ichiban('A', A=1, B=9).kuji()
-            10.0
-            >>> Ichiban('A', 'B', A=1, B=9, C=90).kuji()
-            10.0
-            >>> Ichiban('A', 'B', 'C', A=1, B=9, C=90, D=100).kuji()
-            50.0
-            >>> Ichiban('A', A=1, B=9, C=90, D=100).kuji()
-            0.5
-
-        args:
-            hit: 当たりくじの賞
-            remain: 現在余っているくじ
-
-        return: 当たりくじが当たる確率
+    def atari(self, *hit):
+        """hit: 当たりくじ
+        >>> Ichiban(A=1, B=9, C=90).atari('A', 'B')
+        {'A': 1, 'B': 9}
         """
-        return 100 * sum(self.atari) / sum(self.nokori.values())
+        return {h: self.nokori[h] for h in hit}
+
+    def kuji(self, *hit):
+        """当たりくじを引く確率
+        >>> Ichiban(A=1, B=9).kuji('A')
+        10.0
+        """
+        return 100 * sum(self.atari(*hit).values()) / sum(self.nokori.values())
 
     def all(self):
-        """残りくじの数"""
+        """残りくじの数
+        >>> Ichiban(S=1, A=2, B=2).all()
+        5
+        """
         return sum(self.nokori.values())
 
 
