d23e8e07255941f8199ec3a1cc49494e2896a59f
u1and0 <e01.ando60@gmail.com>
2018-07-11 23:25:06 +0900

    [mod] !!BREAKABLE CHANGE!! Use UserList instead of UserDict. split file caldict.py

--- ichiban.py
+++ ichiban.py
@@ -1,345 +1,84 @@
 #!/usr/bin/env python3
 """
-# CalDict
-
-* Caluculatable dictionary with operator
-* python dictionary enhancement
-* inherit of UserDict
-
 # Ichiban
 Ichiban kuji simulator
 """
-from collections import UserDict
+from collections import UserList
 import random
 
 
-class CalDict(UserDict):
-    """Caluculatable dictionary with operator
-    usage:
-        # calculate
-        >>> cdic = CalDict(a=1, b=5, c=15)
-        >>> cdic + 5
-        {'a': 6, 'b': 10, 'c': 20}
-        >>> cdic - 5
-        {'a': -4, 'b': 0, 'c': 10}
-        >>> cdic * 5
-        {'a': 5, 'b': 25, 'c': 75}
-        >>> cdic / 5
-        {'a': 0.2, 'b': 1.0, 'c': 3.0}
-        >>> cdic // 5
-        {'a': 0, 'b': 1, 'c': 3}
-        >>> cdic % 5
-        {'a': 1, 'b': 0, 'c': 0}
-        >>> cdic ** 5
-        {'a': 1, 'b': 3125, 'c': 759375}
-        >>> -cdic
-        {'a': -1, 'b': -5, 'c': -15}
-        >>> cdic > 0
-        True
-        >>> cdic > 1
-        False
-        >>> -cdic <= -1
-        True
-
-        # self calculate
-        >>> cdic = CalDict(a=1, b=5, c=15)
-        >>> cdic -= 5
-        >>> cdic
-        {'a': -4, 'b': 0, 'c': 10}
-
-        # reverse caluculate
-        >>> cdic = CalDict(a=2, b=5, c=10)
-        >>> 2 / cdic
-        {'a': 1.0, 'b': 0.4, 'c': 0.2}
-
-        # element add
-        >>> cdic = CalDict(a=1, b=-1, c=15)
-        >>> bdic = CalDict(a=1, b=1, c=1)
-        >>> cdic += bdic
-        >>> cdic  # all key
-        {'a': 2, 'b': 0, 'c': 16}
-        >>> cdic + {'a': 5, 'c':-5}  # particular keys
-        {'a': 7, 'b': 0, 'c': 11}
-
-        # slice
-        >>> cdic = CalDict(a=1, b=5, c=15)
-        >>> cdic['a']  # normal slice
-        1
-        >>> cdic['a','c']  # multiple slice
-        {'a': 1, 'c': 15}
-        >>> list(cdic['a','c'].values())  # get list values
-        [1, 15]
-
-        # function apply
-        >>> cdic = CalDict(a=1, b=5, c=15)
-        >>> cdic.apply(sum)
-        21
-        >>> cdic.apply(lambda x: x**2)
-        {'a': 1, 'b': 25, 'c': 225}
-        >>> cdic.apply(lambda x,y,z: x*y*z, 10, 0.5)
-        {'a': 5.0, 'b': 25.0, 'c': 75.0}
-        >>> cdic.apply([max,min])
-        {'max': 15, 'min': 1}
-
-        # stats
-        >>> cdic = CalDict(a=1, b=5, c=15)
-        >>> cdic.max()
-        15
-        >>> cdic.min()
-        1
-        >>> cdic.sum()
-        21
-        >>> cdic.mean()
-        7.0
-    """
-
-    def __init__(self, **kwargs):
-        super().__init__(self, **kwargs)
-
-    def __add__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: self[i] + value[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: v + value for k, v in self.items()})
-
-    def __radd__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: value[i] + self[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: value + v for k, v in self.items()})
-
-    def __sub__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: self[i] - value[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: v - value for k, v in self.items()})
-
-    def __rsub__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: value[i] - self[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: value - v for k, v in self.items()})
-
-    def __mul__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: self[i] * value[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: v * value for k, v in self.items()})
-
-    def __rmul__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: value[i] * self[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: value * v for k, v in self.items()})
-
-    def __truediv__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: self[i] / value[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: v / value for k, v in self.items()})
-
-    def __rtruediv__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: value[i] / self[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: value / v for k, v in self.items()})
-
-    def __floordiv__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: self[i] // value[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: v // value for k, v in self.items()})
-
-    def __rfloordiv__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: value[i] // self[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: value // v for k, v in self.items()})
-
-    def __mod__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: self[i] % value[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: v % value for k, v in self.items()})
-
-    def __rmod__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: value[i] % self[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: value % v for k, v in self.items()})
-
-    def __pow__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: self[i]**value[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: v**value for k, v in self.items()})
-
-    def __rpow__(self, value):
-        try:
-            dic = self.copy()
-            if isinstance(value, dict) or isinstance(value.data, dict):
-                dic.update(**{i: value[i]**self[i] for i in value.keys()})
-            return dic
-        except AttributeError:
-            return CalDict(**{k: value**v for k, v in self.items()})
-
-    def __neg__(self):
-        return CalDict(**{k: -v for k, v in self.items()})
-
-    def __gt__(self, value):
-        return all(i > value for i in self.values())
-
-    def __lt__(self, value):
-        return all(i < value for i in self.values())
-
-    def __ge__(self, value):
-        return all(i >= value for i in self.values())
-
-    def __le__(self, value):
-        return all(i <= value for i in self.values())
-
-    def __getitem__(self, key):
-        if len(key) < 2:
-            return self.data[key]
-        return CalDict(**{i: self.data[i] for i in key})
-
-    def apply(self, func, *args, **kwargs):
-        """Using one or more operations over keys
-        usage:
-            cdic = CalDict(a=1, b=5, c=15)
-            cdic.apply(sum)
-            21
-            cdic.apply(lambda x: x**2)
-            {'a': 1, 'b': 25, 'c': 225}
-            cdic.apply(lambda x,y,z: x*y*z, 10, 0.5)
-            {'a': 5.0, 'b': 25.0, 'c': 75.0}
-            cdic.apply([max,min])
-            {'max': 15, 'min': 1}
-        """
-        if isinstance(func, (list, tuple)):
-            return CalDict(
-                **{f.__name__: self.apply(f, *args, **kwargs)
-                   for f in func})
-        else:
-            try:
-                return CalDict(
-                    **{k: func(v, *args, **kwargs)
-                       for k, v in self.items()})
-            except (ValueError, AttributeError, TypeError):
-                return func(self.values(), *args, **kwargs)
-
-    def max(self):
-        """return max of values"""
-        return self.apply(max)
-
-    def min(self):
-        """return min of values"""
-        return self.apply(min)
-
-    def sum(self):
-        """return sum of values"""
-        return self.apply(sum)
-
-    def mean(self):
-        """return mean of values"""
-        return self.apply(sum) / len(self)
-
-
-class Ichiban(CalDict):
+class Ichiban(UserList):
     """一番くじ確率計算
     usage:
         >>> poke = Ichiban(A=1, B=2, C=2)  # 残り景品の数を入力
-        >>> poke  # 残り景品とその数
+        >>> poke.data  # 残り景品
+        ['A', 'B', 'B', 'C', 'C']
+        >>> poke  # 残り景品とその数(__repr__で返される)
         {'A': 1, 'B': 2, 'C': 2}
-        >>> poke.kuji()  # くじを引く確率
+        >>> poke.dict()  # 残り景品とその数(dict型で返される)
+        {'A': 1, 'B': 2, 'C': 2}
+        >>> len(poke)  # 残り景品の全数
+        5
+        >>> poke.kuji()  # それぞれの景品を引く確率
         {'A': 0.2, 'B': 0.4, 'C': 0.4}
-        >>> poke.kuji('A', 'B')  # A賞, B賞を引くそれぞれの確率
-        {'A': 0.2, 'B': 0.4}
-        >>> round(poke.kuji('A', 'B').sum(), 1)  # A賞またはB賞を引く確率
+        >>> poke.kuji('A', 'B')  # A賞またはB賞を引く確率
         0.6
-        >>> poke.kakaku(A=2000, B=1000, C=100)  # 期待値計算
+        >>> poke.kakaku(A=2000, B=1000, C=100)  # 期待値計算, 景品に価格を付ける
         840.0
-        >>> poke.hiku('A')  # A賞を引いて残数を1減らす(引数が無ければ景品ランダム)
-        'A'
+        >>> poke.kakaku(A=2000)  # 期待値計算, 引数が足りなければdefaultに指定された価格(default=0)
+        400.0
+        >>> poke.hiku('B')  # B賞を引いて残数を1減らす(引数が無ければランダムに景品を選択)
+        'B'
         >>> poke.kuji()  # A賞を引いた後の確率
-        {'A': 0.0, 'B': 0.5, 'C': 0.5}
+        {'A': 0.25, 'B': 0.25, 'C': 0.5}
+        >>> len(poke)  # A賞を引いた後の全数
+        4
 
-    args:
-        remain: 現在余っている景品とその数
+    args: remain:
+        現在余っている景品とその数
+    self.data:
+        リスト型 (__repr__では辞書型に見えるが、メソッドはリスト型を継承)
+    self.dict():
+        dict typeとして返す。valuesは個数。
+    self.__repr__():
+        インスタンスを打った時にdictのように表示する。
+    self.kuji(self, *keys):
+        景品を引く確率計算
+    self.kakaku(self, default=0, **val):
+        期待値計算
+    self.hiku(self, key=None):
+        くじ引きシミュレート
     """
 
     def __init__(self, **remain):
-        super().__init__(**remain)
+        super().__init__(self)
+        self.data = [x for k, v in remain.items() for x in list(k * v)]
+
+    def dict(self):
+        """return dict type data"""
+        return {k: self.count(k) for k in sorted(list(set(self)))}
+
+    def __repr__(self):
+        return self.dict().__repr__()
 
     def kuji(self, *keys):
         """景品を引く確率計算"""
-        if not keys:
-            return self / self.sum()
-        return self[keys] / self.sum()
+        if keys:
+            # float type
+            return sum([self.count(k) for k in set(keys)]) / len(self)
+        return {k: self.count(k) / len(self) for k in self}  # dict type
 
-    def kakaku(self, **values):
+    def kakaku(self, default=0, **val):
         """期待値計算"""
-        kakaku_dic = self.kuji() * values
-        return kakaku_dic.sum()
+        prob = self.kuji()
+        kakaku_dic = {k: val.get(k, default) for k in prob.keys()}
+        kitai_dic = {k: prob[k] * kakaku_dic[k] for k in kakaku_dic.keys()}
+        return sum(kitai_dic.values())
 
     def hiku(self, key=None):
         """くじ引きシミュレート"""
-        if key:
-            self[key] -= 1
-            if self < 0:
-                self[key] += 1
-                raise KeyError('{} is 0. Choose other key.'.format(key))
-        else:
-            while True:
-                key = random.choice(list(self.keys()))
-                self[key] -= 1
-                if self > 0:
-                    break
-                elif self <= 0:
-                    self[key] += 1
-                    raise KeyError('All values are 0.')
-                else:
-                    self[key] += 1
-        return key
+        index = self.index(key if key else random.choice(self))
+        return self.pop(index)
 
 
 if __name__ == '__main__':
